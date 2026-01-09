0edef20c8908007bd8d896a7ecbafd10ef1f07f4
kishore3siddireddy <krishnamurthy.siddireddy@gmail.com>
2026-01-09 23:29:36 +0530

    makeing updates and add some pdf files

--- src/pages/Sub.jsx
+++ src/pages/Sub.jsx
@@ -16,7 +16,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo} alt="Card image cap"/> */}
                           <div class="card-body">
-                             <h5 class="card-title">CO</h5>
+                             <h3 class="card-title">CO</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
@@ -29,7 +29,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo2} alt="Card image cap"/> */}
                           <div class="card-body">
-                             <h5 class="card-title">OS</h5>
+                             <h3 class="card-title">OS</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
@@ -43,7 +43,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo3} alt="Card image cap" /> */}
                           <div class="card-body">
-                             <h5 class="card-title">A/C</h5>
+                             <h3 class="card-title">A/C</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
@@ -63,7 +63,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo4} alt="Card image cap"/> */}
                           <div class="card-body">
-                             <h5 class="card-title">C/Ds</h5>
+                             <h3 class="card-title">C/Ds</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
@@ -78,7 +78,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo6} alt="Card image cap"/> */}
                           <div class="card-body">
-                             <h5 class="card-title">DAA</h5>
+                             <h3 class="card-title">DAA</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
@@ -92,7 +92,7 @@ function Sub() {
                       <div className="card" style={{ width: "18rem" }}>
                         {/* <img className="card-img-top" src={logo5} alt="Card image cap" /> */}
                           <div class="card-body">
-                             <h5 class="card-title">DMS</h5>
+                             <h3 class="card-title">DMS</h3>
                               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                               <a href="#" class="btn btn-primary">Go somewhere</a>
                           </div>
