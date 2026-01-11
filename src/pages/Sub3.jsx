a4818095337e13b7513a83531c2079bb3729ec19
kishore3siddireddy <krishnamurthy.siddireddy@gmail.com>
2026-01-11 22:40:54 +0530

    changes

--- src/pages/Sub3.jsx
+++ src/pages/Sub3.jsx
@@ -3,6 +3,19 @@ import DMS from '../assets/DMSnotes.pdf';
 import CO from '../assets/coNotes.pdf';
 function Sub3() {
     const [activePdf, setActivePdf] = useState(null)
+
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+
+     const openPdf = (pdf) => {
+    if (isMobile) {
+      // Mobile → open in new tab
+      window.open(pdf, "_blank")
+    } else {
+      // Desktop → open in iframe
+      setActivePdf(pdf)
+    }
+  }
+
   return (
     <div>
         <div className="container">
@@ -12,12 +25,12 @@ function Sub3() {
                      <Card title="DMS" onView={() => setActivePdf(DMS)} />
                </div>
                 {/* PDF Viewer */}
-                {activePdf && (
+                {!isMobile && activePdf && (
         <div className="mt-4">
           <iframe
             src={`${activePdf}#toolbar=0&navpanes=0`}
             width="100%"
-            height="1000px"
+            height="600px"
             style={{ border: "none" }}
             title="PDF Viewer"
           />
@@ -29,13 +42,23 @@ function Sub3() {
 }
 function Card({ title, onView }) {
   return (
+    // <div className="col-12 col-md-4 mb-4">
+    //   <div className="card" style={{ width: "18rem" }}>
+    //     <div className="card-body">
+    //       <h3 className="card-title">{title}</h3>
+    //       <p className="card-text">
+    //         Click below to view notes.
+    //       </p>
+    //       <button className="btn btn-primary" onClick={onView}>
+    //         View PDF
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
     <div className="col-12 col-md-4 mb-4">
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card">
         <div className="card-body">
-          <h3 className="card-title">{title}</h3>
-          <p className="card-text">
-            Click below to view notes.
-          </p>
+          <h3>{title}</h3>
           <button className="btn btn-primary" onClick={onView}>
             View PDF
           </button>
